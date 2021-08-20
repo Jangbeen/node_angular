4e400721f4884f7adb8d90b1bff36b6c78e4747a
JB Park <pab2060@gmail.com>
2021-08-20 16:15:37 -0600

    authentication

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
 const config = require("./config/key");
+const cookieParser = require("cookie-parser");
+const { auth } = require("./middleware/auth");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const { User } = require("./models/User");
 const mongoose = require("mongoose");
+const { urlencoded } = require("body-parser");
+
 mongoose
   .connect(config.mongoURL, {
     useNewUrlParser: true,
@@ -21,12 +26,58 @@ mongoose
 
 app.get("/", (req, res) => res.send("hello"));
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
+
   user.save((err, doc) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
   }); //mogodb method
+});
+
+app.post("/api/users/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, userInfo) => {
+    if (!userInfo) {
+      return res.json({
+        loginSuccess: false,
+        message: "Invaild Email Address",
+      });
+    }
+
+    userInfo.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "Invaild Password",
+        });
+      }
+
+      userInfo.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res.cookie("x_auth", user.token).status(200).json({
+          loginSuccess: true,
+          userId: user._id,
+        });
+      });
+    });
+  });
+});
+
+app.get("/api/users/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    email: req.user.email,
+    isAdmin: req.user.role === 0 ? false : true,
+  });
+});
+
+app.get("/api/users/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => console.log(`Example app is listening on port ${port}`));
