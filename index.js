f5e16a9a439bcc36a756d4befd81f9383eedb77e
JB Park <pab2060@gmail.com>
2021-08-19 22:10:06 -0600

    sercurity

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const config = require("./config/key");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const { User } = require("./models/User");
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://jbpark:Qkrwkdqls!95@learningmongo.pvlvk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("hello"));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  }); //mogodb method
+});
 
 app.listen(port, () => console.log(`Example app is listening on port ${port}`));
